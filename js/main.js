28d618ee98d91f106ff5b5adad9cb4d2997387a1
Ville Tainio <ville.tainio@futurice.com>
2014-09-29 11:34:42 +0300

    Better handling for Github icons

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 
 $(document).ready(function () {
 
-	{% for project in site.projects %}
+	{% for category in site.projects %}
+	{% for project in category.contains %}
 	
 	// Get information about the repository from Github
 	var repouri = "https://api.github.com/repos/futurice-oss/{{project.github_repo_name}}";
@@ -15,12 +16,17 @@ $(document).ready(function () {
 		success: function(data) {
 			$('#stars_{{project.github_repo_name}}').html(data.stargazers_count);
 			$('#forks_{{project.github_repo_name}}').html(data.forks_count);
+
+			// Show the icons.
+			$('#icon_stars_{{project.github_repo_name}}').css('visibility', 'visible');
+			$('#icon_forks_{{project.github_repo_name}}').css('visibility', 'visible');
 		},
 		error: function(req, status, err) {
-			// Error occured so hide the icons.
+			// Error occured so make sure the icons are hidden.
 			$('#icon_stars_{{project.github_repo_name}}').css('visibility', 'hidden');
 			$('#icon_forks_{{project.github_repo_name}}').css('visibility', 'hidden');
 		}});
 
+	{% endfor %}
 	{% endfor %}
 });
